ec381be7ba83e5b92e69271a4d78c50585753e5c
Md. Ashraful Islam <imdashraful17@gmail.com>
2022-07-17 18:42:59 +0600

    First working release

--- guiprint/guiprint_core.py
+++ guiprint/guiprint_core.py
@@ -1,4 +1,25 @@
 """Main module."""
 
-def demoworld():
-    print("Hello Demo 3 World")
+from PyQt5.QtWidgets import QMessageBox, QApplication, QInputDialog, QWidget
+
+def print(msgText):
+	app = QApplication([])
+	msg = QMessageBox()
+	msg.setText(msgText)
+	msg.exec()
+	del app
+
+def input(promptText):
+	app = QApplication([])
+	inp, ok = QInputDialog.getText(None, "GUIPRINT", promptText)
+	if ok:
+		return inp
+	return None
+
+if __name__ == "__main__":
+	# Some debugging codes :)
+	"""
+	print("Hello")
+	msg = input("What's your name? :")
+	print(msg)
+	"""
